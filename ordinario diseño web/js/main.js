8f1ceac9cbd17f9ceb6659ae4647279d4d152a5b
arumando <armandobandera8@gmail.com>
2026-06-19 10:32:15 -0600

    actualizacion
    se agregaron mas paginas

--- ordinario diseño web/js/main.js
+++ ordinario diseño web/js/main.js
@@ -5,3 +5,15 @@ function saludar() {
 function verDetalles(nombreMateria) {
     alert("Has seleccionado la materia: " + nombreMateria + ".\nAquí mostraremos los créditos y el temario próximamente.");
 }
+
+function validarFormulario(evento) {
+    evento.preventDefault();
+    
+    let nombre = document.getElementById('nombre').value;
+    
+    if (nombre !== "") {
+        alert("¡Excelente " + nombre + "! Tus datos han sido enviados correctamente.");
+        document.getElementById('nombre').value = "";
+        document.getElementById('correo').value = "";
+    }
+}
