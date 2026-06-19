0cca09994336b77fa6039a5084f582f0a1a02031
arumando <armandobandera8@gmail.com>
2026-06-19 10:23:46 -0600

    dd
    d

---
+++ ordinario diseño web/js/main.js
@@ -0,0 +1,7 @@
+function saludar() {
+    alert("¡Bienvenido al nuevo portal de Smart Systems!");
+}
+
+function verDetalles(nombreMateria) {
+    alert("Has seleccionado la materia: " + nombreMateria + ".\nAquí mostraremos los créditos y el temario próximamente.");
+}
